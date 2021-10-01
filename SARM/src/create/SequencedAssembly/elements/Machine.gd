49d42ef3a423bb6910b2a26cf75e7cbfda681eab
GibberishDev <gibberishdevelopment@gmail.com>
2021-10-01 22:01:49 +0300

    tied up machine dictionary with graphical representation, linked button number to number of machines

--- SARM/src/create/SequencedAssembly/elements/Machine.gd
+++ SARM/src/create/SequencedAssembly/elements/Machine.gd
@@ -5,24 +5,23 @@ var recipe_text = ""
 
 var machine_number = int()
 
-var TransitionalItem = "\"test:tiph\"" #tiph aka transitional item placeholder
-
 #custom variables
 #saw
 var saw_processing_time = int()
+var saw_processing_time_default = int(20)
 #spout
 var spout_fluid_identifier = ""
+var spout_fluid_identifier_default = ":"
 var spout_fluid_nbt = "{}"
 var spout_fluid_amount = int()
+var spout_fluid_amount_default = int(250)
 #deployer
-var deployer_addition_item = "\"item\": \"test:aiph\"" #aiph aka addition item placeholder
+var deployer_addition_item = ""
+var deployer_addition_item_default = ":"
 
-signal RecipeText(MachineNumber, RecipeType, Text)
+signal RecipeText(MachineNumber, Text)
 
-func _ready():
-	changeRecipeEditor(0)
-
-func changeRecipeEditor(type: int) -> void:
+func changeRecipeEditor(type: int, SPT: int, SFI: String, SFA: int, DAI: String) -> void:
 	match type:
 		0:
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/PressRecipe.visible = true
@@ -30,16 +29,28 @@ func changeRecipeEditor(type: int) -> void:
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SpoutRecipe.visible = false
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/DeployerRecipe.visible = false
 		1:
+			saw_processing_time = SPT
+			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SawRecipe/VBoxContainer/Control/HBoxContainer/TextEdit.text = str(SPT)
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/PressRecipe.visible = false
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SawRecipe.visible = true
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SpoutRecipe.visible = false
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/DeployerRecipe.visible = false
 		2:
+			spout_fluid_identifier = SFI
+			spout_fluid_amount = SFA
+			var Spout_data = SFI.split(":", true, 1)
+			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SpoutRecipe/VBoxContainer/Control/VBoxContainer/HBoxContainer/VBoxContainer/Control/HBoxContainer/modid.text = Spout_data[0]
+			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SpoutRecipe/VBoxContainer/Control/VBoxContainer/HBoxContainer/VBoxContainer/Control/HBoxContainer/identifier.text = Spout_data[1]
+			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SpoutRecipe/VBoxContainer/Control/VBoxContainer/HBoxContainer/VBoxContainer/Control2/HBoxContainer/TextEdit.text = str(SFA)
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/PressRecipe.visible = false
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SawRecipe.visible = false
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SpoutRecipe.visible = true
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/DeployerRecipe.visible = false
 		3:
+			deployer_addition_item = DAI
+			var Deployer_data = DAI.split(":", true, 1)
+			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/DeployerRecipe/VBoxContainer/Control/VBoxContainer/HBoxContainer/VBoxContainer/Control/HBoxContainer/modid.text = Deployer_data[0]
+			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/DeployerRecipe/VBoxContainer/Control/VBoxContainer/HBoxContainer/VBoxContainer/Control/HBoxContainer/identifier.text = Deployer_data[1]
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/PressRecipe.visible = false
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SawRecipe.visible = false
 			$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SpoutRecipe.visible = false
@@ -47,17 +58,7 @@ func changeRecipeEditor(type: int) -> void:
 
 func changeRecipeType(index: int):
 	recipe_type = index
-	changeRecipeEditor(index)
-	saw_processing_time = 20
-	$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SawRecipe/VBoxContainer/Control/HBoxContainer/TextEdit.text = "20"
-	spout_fluid_identifier = ""
-	spout_fluid_amount = 1000
-	$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SpoutRecipe/VBoxContainer/Control/VBoxContainer/HBoxContainer/VBoxContainer/Control/HBoxContainer/modid.text = ""
-	$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SpoutRecipe/VBoxContainer/Control/VBoxContainer/HBoxContainer/VBoxContainer/Control/HBoxContainer/identifier.text = ""
-	$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/SpoutRecipe/VBoxContainer/Control/VBoxContainer/HBoxContainer/VBoxContainer/Control2/HBoxContainer/TextEdit.text = "1000"
-	deployer_addition_item = ""
-	$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/DeployerRecipe/VBoxContainer/Control/VBoxContainer/HBoxContainer/VBoxContainer/Control/HBoxContainer/modid.text = ""
-	$VBoxContainer/Control/Control/VBoxContainer/Control/VBoxContainer/VBoxContainer/DeployerRecipe/VBoxContainer/Control/VBoxContainer/HBoxContainer/VBoxContainer/Control/HBoxContainer/identifier.text = ""
+	changeRecipeEditor(index, saw_processing_time_default, spout_fluid_identifier_default, spout_fluid_amount_default, deployer_addition_item_default)
 
 func sendText():
 	match recipe_type:
@@ -68,23 +69,23 @@ func sendText():
 		2:
 			makeSpoutRecipe()
 		3:
-			pass
-	emit_signal("RecipeText", machine_number, recipe_type, recipe_text)
+			makeDeployerRecipe()
+	emit_signal("RecipeText", machine_number, recipe_text)
 
 func makePressRecipe():
 	recipe_text = ""
 	recipe_text = """		{
 			"type": "create:pressing",
-			"ingredients": [{\"item\": """ + TransitionalItem + """}],
-			"results": [{\"item\": """ + TransitionalItem + """}]
+			"ingredients": [{\"item\": TRPH}],
+			"results": [{\"item\": TRPH}]
 		}"""
 
 func makeSawRecipe():
 	recipe_text = ""
 	recipe_text = """		{
 			"type": "create:cutting",
-			"ingredients": [{\"item\": """ + TransitionalItem + """}],
-			"results": [{\"item\": """ + TransitionalItem + """}],
+			"ingredients": [{\"item\": TRPH}],
+			"results": [{\"item\": TRPH}],
 			"processingTime": """ + str(saw_processing_time) + """
 		}"""
 
@@ -93,16 +94,17 @@ func makeSpoutRecipe():
 	recipe_text = """		{
 			"type": "create:filling",
 			"ingredients": [
-				{\"item\": """ + TransitionalItem + """},
+				{\"item\": TRPH},
 				{"fluid": """ + spout_fluid_identifier + """, "nbt": """ + spout_fluid_nbt + """, "amount": """ + str(spout_fluid_amount) + """}],
-			"results": [{\"item\": """ + TransitionalItem + """}]
+			"results": [{\"item\": TRPH}]
 		}"""
 
 func makeDeployerRecipe():
 	recipe_text = ""
 	recipe_text = """		{
 			"type": "create:deploying",
-			"ingredients": [{\"item\": """ + TransitionalItem + """}, {""" + deployer_addition_item + """}],
-			"results": [{\"item\": """ + TransitionalItem + """}]
+			"ingredients": [{\"item\": TRPH}, {""" + deployer_addition_item + """}],
+			"results": [{\"item\": TRPH}]
 		}"""
+
 
--- SARM/src/create/SequencedAssembly/elements/SequencePreview.gd
+++ SARM/src/create/SequencedAssembly/elements/SequencePreview.gd
@@ -7,7 +7,9 @@ var viewport = "Control/bgOverlay/MainFrame/HBC/VBoxContainer/mainMenu/SequenceP
 var pathToSeqButton = "/HBoxContainer/Control/sequenceBtn"
 var pathToControls = "/HBoxContainer/Control/HBoxContainer"
 
-var SequenceTypes = [0] #0 - press recipe, 1 - saw recipe, 2 - spout recipe, 3 - deployer recipe
+var SequenceTypes = [0, -1, -1, -1, -1, -1, -1, -1] #0 - press recipe, 1 - saw recipe, 2 - spout recipe, 3 - deployer recipe, -1 - null
+
+export(PackedScene) var MachineRecipe
 
 #SequenceMechsDictionary, type: 0 - press, 1 - saw, 2 - spout, 3 - deployer
 var SMD = {
@@ -26,8 +28,15 @@ var SMD = {
 
 func _ready():
 	updateSequenceSprites()
+	var ThisNode = self
+	
 
 func updateSequenceSprites() -> void:
+	for i in range(1, 9):
+		if SMD[str(i)].active == true:
+			SequenceTypes[i-1] = SMD[str(i)].type
+		else:
+			SequenceTypes[i-1] = -1
 	$HBoxContainer/visualizerContainer/ViewportContainer/Viewport/VisualisationNode.ChangeModels(SequenceTypes)
 
 func _on_Control_resized():
@@ -40,43 +49,44 @@ func toggleBtn_2() -> void:
 	get_node(pathToButtons + str(2) + pathToSeqButton).visible = !get_node(pathToButtons + str(2) + pathToSeqButton).visible
 	get_node(pathToButtons + str(2) + pathToControls).visible = !get_node(pathToButtons + str(2) + pathToControls).visible
 	get_node(pathToButtons + str(3) + pathToControls).visible = !get_node(pathToButtons + str(3) + pathToControls).visible
+	SMD["2"].active = !SMD["2"].active
+	updateSequenceSprites()
 func toggleBtn_3() -> void:
 	get_node(pathToButtons + str(3) + pathToSeqButton).visible = !get_node(pathToButtons + str(3) + pathToSeqButton).visible
 	get_node(pathToButtons + str(3) + pathToControls).visible = !get_node(pathToButtons + str(3) + pathToControls).visible
 	get_node(pathToButtons + str(4) + pathToControls).visible = !get_node(pathToButtons + str(4) + pathToControls).visible
+	SMD["3"].active = !SMD["3"].active
+	updateSequenceSprites()
 func toggleBtn_4() -> void:
 	get_node(pathToButtons + str(4) + pathToSeqButton).visible = !get_node(pathToButtons + str(4) + pathToSeqButton).visible
 	get_node(pathToButtons + str(4) + pathToControls).visible = !get_node(pathToButtons + str(4) + pathToControls).visible
 	get_node(pathToButtons + str(5) + pathToControls).visible = !get_node(pathToButtons + str(5) + pathToControls).visible
+	SMD["4"].active = !SMD["4"].active
+	updateSequenceSprites()
 func toggleBtn_5() -> void:
 	get_node(pathToButtons + str(5) + pathToSeqButton).visible = !get_node(pathToButtons + str(5) + pathToSeqButton).visible
 	get_node(pathToButtons + str(5) + pathToControls).visible = !get_node(pathToButtons + str(5) + pathToControls).visible
 	get_node(pathToButtons + str(6) + pathToControls).visible = !get_node(pathToButtons + str(6) + pathToControls).visible
+	SMD["5"].active = !SMD["5"].active
+	updateSequenceSprites()
 func toggleBtn_6() -> void:
 	get_node(pathToButtons + str(6) + pathToSeqButton).visible = !get_node(pathToButtons + str(6) + pathToSeqButton).visible
 	get_node(pathToButtons + str(6) + pathToControls).visible = !get_node(pathToButtons + str(6) + pathToControls).visible
 	get_node(pathToButtons + str(7) + pathToControls).visible = !get_node(pathToButtons + str(7) + pathToControls).visible
+	SMD["6"].active = !SMD["6"].active
+	updateSequenceSprites()
 func toggleBtn_7() -> void:
 	get_node(pathToButtons + str(7) + pathToSeqButton).visible = !get_node(pathToButtons + str(7) + pathToSeqButton).visible
 	get_node(pathToButtons + str(7) + pathToControls).visible = !get_node(pathToButtons + str(7) + pathToControls).visible
 	get_node(pathToButtons + str(8) + pathToControls).visible = !get_node(pathToButtons + str(8) + pathToControls).visible
+	SMD["7"].active = !SMD["7"].active
+	updateSequenceSprites()
 func toggleBtn_8() -> void:
 	get_node(pathToButtons + str(8) + pathToSeqButton).visible = !get_node(pathToButtons + str(8) + pathToSeqButton).visible
 	get_node(pathToButtons + str(8) + pathToControls).visible = !get_node(pathToButtons + str(8) + pathToControls).visible
 	get_node(pathToButtons + "removeBtn/HBoxContainer").visible = !get_node(pathToButtons + "removeBtn/HBoxContainer").visible
-
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("test_input"):
-		match randi() % 4:
-			0:
-				SequenceTypes = [0, 1, 2, 3, 0, 1, 2, 3]
-			1:
-				SequenceTypes = [0, 1, 2, 3]
-			2:
-				SequenceTypes = [0, 1, 2, 3, 3, 3, 2, 1]
-			3:
-				SequenceTypes = [0, 1, 2, 3, 1, 3, 2, 1]
-		updateSequenceSprites()
+	SMD["8"].active = !SMD["8"].active
+	updateSequenceSprites()
 
 func sendSeq() -> void:
 	pass
